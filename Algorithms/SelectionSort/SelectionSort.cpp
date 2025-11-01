f7405962df8118cdcf16449f5197ed752c3c688f
Bogdan <toqwer00@outlook.com>
2025-11-01 17:20:34 +1000

    Solved 283, 215

--- Algorithms/SelectionSort/SelectionSort.cpp
+++ Algorithms/SelectionSort/SelectionSort.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <algorithm>
+#include <queue>
 
 using namespace std;
 
@@ -29,30 +30,7 @@ class SelectionSort {
         }
 };
 
-class SolutionSort {
-    private:
-        void swap(int& a, int& b) {
-            int temp = a;
-            a = b;
-            b = temp;
-        }
 
-    public:
-        void sort(vector<int>& nums) {
-            int n = nums.size();
-
-            for(int i = 0; i < n - 1; i++) {
-                int min_index = i;
-
-                for(int j = i + 1; j < n; j++) {
-                    if (nums[j] < nums[min_index]) {
-                        min_index = j;
-                    }
-                }
-                swap(nums[i], nums[min_index]);
-            }
-        }
-};
 
    
 
@@ -67,6 +45,46 @@ class Solution {
                 if (nums[i] != 0) {
                     swap(nums[nonZeroPos++], nums[i]);
                 }
+            }
+        }
+};
+
+
+
+class Solution {
+    public:
+        void moveZeroes(vector<int>& nums) {
+            int nonZeroPos = 0;
+
+            for(int i = 0; i < nums.size(); i++) {
+                if (nums[i] != 0) {
+                    swap(nums[nonZeroPos++], nums[i]);
+                }
+            }
+        }
+};
+
+class Solution {
+    public:
+        void moveZeroes(vector<int>& nums) {
+            int nonZeroPos = 0;
+
+            for(int i = 0; i < nums.size(); i++) {
+                if (nums[i] != 0) 
+                    swap(nums[nonZeroPos++], nums[i]);
+            }
+        }
+};
+
+
+class Solution {
+    public:
+        void moveZeroes(vector<int>& nums) {
+            int nonZeroPos = 0;
+
+            for(int i = 0; i < nums.size(); i++) {
+                if (nums[i] != 0)
+                    swap(nums[nonZeroPos++], nums[i]);
             }
         }
 };
@@ -92,6 +110,169 @@ class Solution {
             return nums[k-1];
         }
 };
+
+
+class Solution {
+    public:
+        int findKLargest(vector<int>& nums, int k) {
+            for(int i = 0; i < k; i++) {
+                int max_index = i;
+
+                for(int j = i + 1; j < nums.size(); j++) {
+                    
+                    if (nums[j] > nums[max_index])
+                        max_index  = j;
+                }
+
+                swap(nums[i], nums[max_index]);
+            }
+
+            return nums[k - 1];
+        }
+};
+
+
+
+
+
+
+// Не оптимальное решение т.к. Время: O(n * k)
+//                             Память: O(1)
+class Solution {
+    public: 
+        int findKLargest(vector<int>& nums, int k) {
+            for(int i = 0; i < k; i++) {
+                int max_index = i;
+
+                for(int j = i + 1; j < nums.size(); j++) {
+                    if (nums[j] > nums[max_index])
+                        max_index = j;
+                }
+
+                swap(nums[i], nums[max_index]);
+            }
+
+            return nums[k - 1];
+        }
+};
+
+// Более оптимальное решение: 
+// QuickSelect(O (n) в среднем) 
+class Solution {
+    public:
+        int findKthLargest(vector<int>& nums, int k) {
+            return quickSelect(nums, 0, nums.size() - 1, k);
+        }
+
+     
+
+    private:
+        int quickSelect(vector<int>& nums, int left, int right, int k){
+            int pivot = partition(nums, left, right);
+
+            if (pivot == k - 1) 
+                return nums[pivot];
+
+            else if (pivot > k - 1) 
+                return quickSelect(nums, left, pivot - 1, k);
+
+            else 
+                return quickSelect(nums, pivot + 1, right, k);
+        }
+
+
+      int quickSelect(vector<int>& nums, int right, int left, int k) {
+        int pivot = partition(nums, right, left);
+
+        if (pivot == k - 1)
+            return nums[pivot];
+        
+        else if (pivot < k - 1)
+            return quickSelect(nums, left, k - 1, k);
+        
+        else    
+            return quickSelect(nums, k + 1, right, k);
+      }
+
+
+        int partition(vector<int>& nums, int left, int right) {
+            int pivot = nums[right];    
+            int i = left;
+            for(int j = left; j < right; j++) {
+                if (nums[j] >= pivot) {
+                    swap(nums[i], nums[j]);
+                    i++;
+                }
+            }
+            swap(nums[i], nums[right]);
+            return i;
+        }   
+};
+
+
+
+
+
+// Min-Heap (O (n log k)):
+
+class Solution {
+    public:
+        int findKthLargest(vector<int>& nums, int k ) {
+            priority_queue<int, vector<int>, greater<int>> minHeap;
+
+            for(int num : nums) {
+                minHeap.push(num);
+                    if(minHeap.size() > k) {
+                        minHeap.pop();
+                    }
+            }
+            return minHeap.top();
+        }
+};
+
+
+class Solution {
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+
+        for(int num : nums) {
+            minHeap.push(num);
+
+            if (minHeap.size() > k) {
+                minHeap.pop();
+            }
+        }
+        return minHeap.top();
+    }
+};
+
+
+
+// STL nth_element
+
+class Soltuion {
+    public:
+        int findKthLargest(vector<int>& nums, int k) {
+
+            nth_element(nums.begin(), nums.begin() + k - 1, 
+            nums.end(), greater<int>());
+            
+            return nums[k - 1];
+        }
+};
+
+
+class Solution {
+    public:
+        int findKthLargest(vector<int>& nums, int k) {
+            nth_element(nums.begin(), nums.begin() + k - 1, nums.end(), greater<int>());
+            
+            return nums[k - 1];
+        }   
+};
+
+
+
 
 // O(n log n)
 
